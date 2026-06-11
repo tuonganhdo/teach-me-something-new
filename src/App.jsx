7df3580c5cc670b2b1d77c58a15f3477ddf16563
tuonganhdo <anhdotuong@gmail.com>
2026-06-11 15:58:25 +0700

    Added heading, turned learn more link into button

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const observerTarget = useRef(null);
 
   const fetchFact = useCallback(async () => {
+    setLoading(true);
     try {
       const response = await fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary")
       const data = await response.json();
@@ -45,8 +46,11 @@ function App() {
 
   return (
     <>
-      <main id='app-container' className='flex flex-col gap-y-4 py-4'>
-        <div id='facts-feed' className="flex flex-col gap-y-4">
+      <main id='app-container' className='flex flex-col pb-4'>
+        <div id='header' className='sticky top-0 left-0 px-5 py-3 w-full border-b border-gray-300 bg-white'>
+          <p className='font-medium text-gray-700 justify-center flex text-sm'>Teach me something new</p>
+        </div>
+        <div id='facts-feed' className="flex flex-col gap-y-4 my-4 w-[90vw] mx-auto">
           {facts.map((fact, index) => (
             <FactCard fact={fact} key={index}/>
           ))}
@@ -54,7 +58,7 @@ function App() {
 
         <div ref={observerTarget} id='scroll-trigger' className='flex justify-center'>
           {loading ? (
-            <span className="text-gray-400 font-bold">Fetching your next fact...</span>
+            <span className="text-gray-400">Fetching your next fact...</span>
           ) : (
             <span className="text-gray-400">Scroll for more</span>
           )}
--- src/FactCard.jsx
+++ src/FactCard.jsx
@@ -1,13 +1,13 @@
 export default function FactCard({fact}) {
     return(
-        <div id='fact-container' className='flex flex-col md:flex-row border border-gray-300 py-7 px-5 rounded-2xl gap-6'>
+        <div id='fact-container' className='flex flex-col border border-gray-300 py-7 px-5 rounded-2xl w-full gap-6 max-w-[600px] mx-auto'>
             {fact.thumbnail && (
-            <img id='fact-image' className='flex h-full w-full md:w-1/3 lg:w-1/4 rounded-lg' src={fact.thumbnail.source} alt={fact.title}/>
+            <img id='fact-image' className='flex h-full w-full rounded-lg' src={fact.thumbnail.source} alt={fact.title}/>
             )}
             <div id='fact-text-container' className='flex flex-col text-left gap-2'>
             <h2 id='fact-title' className='font-bold text-xl'>{fact.title}</h2>
             <p id='fact-body'>{fact.extract}</p>
-            <a id='fact-link' className='underline text-blue-700 italic' href={fact.content_urls.desktop.page} target='_blank'>Learn more</a>
+            <a id='fact-link' className='bg-blue-700 hover:bg-blue-800 rounded-lg py-3 text-white mt-2 font-medium flex justify-center' href={fact.content_urls.desktop.page} target='_blank'>Learn more</a>
             </div>
         </div>
     )
